634ec322c5648d7920f6893c925eed44da28b7a1
Ralph Rouhana <ralph.rouhana@gmail.com>
2023-08-07 19:56:15 -0400

    Fix build errors

--- app/api/vote/[id]/_handlers/deleteVote.ts
+++ app/api/vote/[id]/_handlers/deleteVote.ts
@@ -4,19 +4,9 @@ import { getServerSession } from 'next-auth';
 import prisma from '@/_lib/server/prismadb';
 import { generateApiResponse } from '@/api/_lib/generateApiResponse';
 import { logErrorMessage } from '@/api/_lib/generateErrorMessage';
-import { authOptions } from '@/api/auth/[...nextauth]/route';
 
 export const deleteVote = async (_: NextRequest, { params: { id } }: { params: { id: string } }) => {
   try {
-    const session = await getServerSession(authOptions);
-
-    if (!session?.user) {
-      return generateApiResponse({
-        status: 401,
-        error: 'Unauthenticated.',
-      });
-    }
-
     await prisma.vote.delete({
       where: {
         id,
